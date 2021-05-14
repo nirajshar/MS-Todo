27b0c0a22f4a87efbb9a917800535040b43f94aa
nirajshar <nirajshar67@gmail.com>
2021-05-14 16:08:58 +0530

    Added Swagger Ui & Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+
 
 async function bootstrap() {
 
@@ -8,6 +11,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('api');
   // app.enableCors();
+  app.use(helmet());  
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo-Service')
+    .setDescription('Todo Service : Stores & Handle User Todo\'s')
+    .setVersion('1.0')    
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document,{
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+
   await app.listen(port);
   
   console.log(`App listening on http://localhost:${port}`);
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,34 +1,54 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { TodoService } from './todo.service';
 
+@ApiTags('Todo')
 @Controller('todo')
 export class TodoController {
     constructor(
         private readonly todoService: TodoService,
-    ) {}
+    ) { }
 
+
+    @ApiOperation({ summary: 'Find all todos' })
+    @ApiResponse({ status: 200, description: 'Return all todos' })
+    @ApiResponse({ status: 404, description: 'Todo not found' })
     @Get()
     async findAll() {
         return await this.todoService.findAll();
     }
 
-    @Get(':id') 
+    @ApiOperation({ summary: 'Get One todo' })
+    @ApiResponse({ status: 200, description: 'Return all todos' })
+    @ApiResponse({ status: 404, description: 'Todo not found' })
+    @Get(':id')
     async findOne(@Param('id') id: string) {
         return await this.todoService.findOne(id);
     }
 
+    @ApiOperation({ summary: 'Create One todo' })
+    @ApiBody({ type: CreateTodoDto })
+    @ApiResponse({ status: 200, description: 'Create one todo' })
+    @ApiResponse({ status: 404, description: 'Todo not created' })
     @Post()
     async create(@Body() createTodoDto: CreateTodoDto) {
         return await this.todoService.create(createTodoDto);
     }
 
+    @ApiOperation({ summary: 'Update One todo' })
+    @ApiBody({ type: UpdateTodoDto })
+    @ApiResponse({ status: 200, description: 'Update one todo' })
+    @ApiResponse({ status: 404, description: 'Todo not updated' })
     @Put(':id')
     async update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
         return await this.todoService.update(id, updateTodoDto);
     }
 
+    @ApiOperation({ summary: 'Delete One todo' })
+    @ApiResponse({ status: 200, description: 'Delete one todo' })
+    @ApiResponse({ status: 404, description: 'Todo not deleted' })
     @Delete(':id')
     async delete(@Param('id') id: string) {
         return await this.todoService.delete(id);
